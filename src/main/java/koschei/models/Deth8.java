77d643110860b66b111589207074cd3114fae19e
Anton Melchakov <anton.melchakov1@gmail.com>
2020-06-29 22:27:42 +0500

    Before code review

---
+++ src/main/java/koschei/models/Deth8.java
@@ -0,0 +1,12 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Deth8 {
+
+    @Override
+    public String toString() {
+        return "\nВот и сказочке конец!\n" + "A кто слушал молодец!";
+    }
+}
